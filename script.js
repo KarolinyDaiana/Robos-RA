1f6b8d7746deb9dcc8ad27c23a600bc469121aa6
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2023-06-30 20:14:33 -0300

    a

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const geral = document.getElementById(texto-geral);
-const roboTodo = document.getElementById(robo-todo);
+const geral = document.getElementById("texto-geral");
+const roboTodo = document.getElementById("robo-todo");
+
+geral.style.display = 'none';
 
 roboTodo.addEventListener("TargetFound", event => {
     geral.style.display = 'block';
