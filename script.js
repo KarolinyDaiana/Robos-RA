4ccb86ea991b90f940b492fcebd5d98a6c0b29b5
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2023-07-07 19:10:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const geral = document.getElementById("texto-geral");
 const roboEnt = document.getElementById("robo-todo");
+
 const fechar = document.getElementsByClassName("fechar");
+
 const garra = document.getElementById("texto-garra");
 const garraEnt = document.getElementById("robo-garra");
+
 const sensor = document.getElementById("texto-sensor");
 const sensorEnt = document.getElementById("robo-sensor");
+
 const motor = document.getElementById("texto-motor");
 const motorEnt = document.getElementById("robo-motor");
-
-fechar.addEventListener('click', function() {
-    console.log("teste");
-    geral.style.display = 'none';
-})
 
 roboEnt.addEventListener("targetFound", event => {
     geral.style.display = 'block';
@@ -28,3 +27,8 @@ sensorEnt.addEventListener("targetFound", event => {
 motorEnt.addEventListener("targetFound", event => {
     motor.style.display = 'block';
 }) 
+
+fechar.addEventListener('click', function() {
+    console.log("teste");
+    geral.style.display = 'none';
+})
