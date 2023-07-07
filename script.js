13907b7879ebfc95488106d2b433035836fe6913
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2023-07-07 19:06:27 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const geral = document.getElementById("texto-geral");
 const roboEnt = document.getElementById("robo-todo");
 const fechar = document.getElementById("fechar");
+const garra = document.getElementById("texto-garra");
+const garraEnt = document.getElementById("robo-garra");
+const sensor = document.getElementById("texto-sensor");
+const sensorEnt = document.getElementById("robo-sensor");
+const motor = document.getElementById("texto-motor");
+const motorEnt = document.getElementById("robo-motor");
 
 fechar.addEventListener('click', function() {
     console.log("teste");
@@ -11,22 +17,13 @@ roboEnt.addEventListener("targetFound", event => {
     geral.style.display = 'block';
 })
 
-const garra = document.getElementById("texto-garra");
-const garraEnt = document.getElementById("robo-garra");
-
 garraEnt.addEventListener("targetFound", event => {
     garra.style.display = 'block';
 })
 
-const sensor = document.getElementById("texto-sensor");
-const sensorEnt = document.getElementById("robo-sensor");
-
 sensorEnt.addEventListener("targetFound", event => {
     sensor.style.display = 'block';
 })
-
-const motor = document.getElementById("texto-motor");
-const motorEnt = document.getElementById("robo-motor");
 
 motorEnt.addEventListener("targetFound", event => {
     motor.style.display = 'block';
